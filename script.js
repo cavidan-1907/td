8f4fee1b505d1636a3c3b9d4483526568aaa8492
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-11-16 23:13:48 +0400

    calc

--- script.js
+++ script.js
@@ -9,30 +9,36 @@ let input = document.getElementById("input");
 
 btn.addEventListener("click", () => {
 
+
     let li = document.createElement("li")
     li.innerText = input.value;
     ul.append(li)
+    let div = document.createElement("div")
+    li.append(div)
     input.value = ""
     let i = document.createElement("i")
     i.className = "bi bi-trash3"
-    li.append(i)
-    let i2=document.createElement("i")
-    i2.className="bi bi-crosshair2"
-    li.append(i2)
-
-
+    div.append(i)
+    let i2 = document.createElement("i")
+    i2.className = "bi bi-crosshair2"
+    div.append(i2)
     i.addEventListener("click", () => {
         li.remove()
     })
+
+
+    i2.addEventListener("click", () => {
+        li.style.textDecoration=line-through;
+    })
+
+
+
 })
 
 
 
 
-i2.addEventListener("click",()  => {
-    li.style.textDecoration= "line-through"
-    
-    })
+
 
 
 
@@ -44,14 +50,26 @@ i2.addEventListener("click",()  => {
 document.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         let li = document.createElement("li")
-    li.innerText = input.value;
-    ul.append(li)
-    input.value = ""
-    let i = document.createElement("i")
-    i.className = "bi bi-trash3"
-    li.append(i)
-    i.addEventListener("click", () => {
-        li.remove()
-    })
+        li.innerText = input.value;
+        ul.append(li)
+        let div = document.createElement("div")
+        li.append(div)
+        input.value = ""
+        let i = document.createElement("i")
+        i.className = "bi bi-trash3"
+        div.append(i)
+        let i2 = document.createElement("i")
+        i2.className = "bi bi-crosshair2"
+        div.append(i2)
+        i.addEventListener("click", () => {
+            li.remove()
+        })
+    
+    
+        i2.addEventListener("click", () => {
+            li.style.textDecoration=line-through;
+        })
     }
 })
+
+
