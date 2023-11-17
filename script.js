88d26f9864a4ddf4935b17cd9c7a1853a67aad29
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-11-17 17:06:48 +0400

    cavidan

--- script.js
+++ script.js
@@ -1,57 +1,74 @@
+
 let btn = document.getElementById("btn");
 let input = document.getElementById("input");
+let ul = document.getElementById("ul"); 
+
+
+
+
+
 btn.addEventListener("click", () => {
-    if (input.value === "")
-    {
-        alert("xahis olunur bir edilecek is elave edin!")
+    if (input.value.trim() === "") {
+        alert("Xahiş olunur bir ediləcək iş əlavə edin!");
         return;
     }
-    let li = document.createElement("li")
+
+    let li = document.createElement("li");
     li.innerText = input.value;
-    ul.append(li)
-    let div = document.createElement("div")
-    li.append(div)
-    input.value = ""
-    let i = document.createElement("i")
-    i.className = "bi bi-trash3"
-    div.append(i)
-    let i2 = document.createElement("i")
-    i2.className = "bi bi-pen"
-    div.append(i2)
+    ul.append(li);
+
+    let div = document.createElement("div");
+    li.append(div);
+
+    input.value = "";
+
+    let i = document.createElement("i");
+    i.className = "bi bi-trash3";
+    div.append(i);
+
+    let i2 = document.createElement("i");
+    i2.className = "bi bi-pen";
+    div.append(i2);
+
     i.addEventListener("click", () => {
-        li.remove()
-    })
-    i2.addEventListener("click", () => {
-        li.style.textDecoration=line-through;
-    })
-})
+        li.remove();
+    });
+    
+
+});
+
+
+
+
+
 document.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
-        if (input.value === "")
-        {
-            alert("xahis olunur bir edilecek is elave edin!")
+        if (input.value.trim() === "") {
+            alert("Xahiş olunur bir ediləcək iş əlavə edin!");
             return;
         }
-        let li = document.createElement("li")
+    
+        let li = document.createElement("li");
         li.innerText = input.value;
-        ul.append(li)
-        let div = document.createElement("div")
-        li.append(div)
-        input.value = ""
-        let i = document.createElement("i")
-        i.className = "bi bi-trash3"
-        div.append(i)
-        let i2 = document.createElement("i")
-        i2.className = "bi bi-pen"
-        div.append(i2)
+        ul.append(li);
+    
+        let div = document.createElement("div");
+        li.append(div);
+    
+        input.value = "";
+    
+        let i = document.createElement("i");
+        i.className = "bi bi-trash3";
+        div.append(i);
+    
+        let i2 = document.createElement("i");
+        i2.className = "bi bi-pen";
+        div.append(i2);
+    
         i.addEventListener("click", () => {
-            li.remove()
-        })
+            li.remove();
+        });
     
-    
-        i2.addEventListener("click", () => {
-            li.style.textDecoration=line-through;
-        })
     }
 })
 
